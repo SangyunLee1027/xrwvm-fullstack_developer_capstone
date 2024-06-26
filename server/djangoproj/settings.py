61f5f1bf7df2893ea37875b927c8ebbe2527fcb4
SangyunLee1027 <1027dltkddbs@gmail.com>
2024-06-26 04:23:47 -0400

    done with car models

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -146,7 +146,7 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MYURL = 'https://u1027dltkddb-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+MYURL = 'https://u1027dltkddb-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ALLOWED_HOSTS=['localhost',MYURL]
 CSRF_TRUSTED_ORIGINS=[MYURL]
 CORS_ALLOWED_ORIGINS=[MYURL]
