6936e876c72687b13016588d1d2840cdf15e26a0
SangyunLee1027 <1027dltkddbs@gmail.com>
2024-06-26 08:34:20 -0400

    fixing

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -153,7 +153,8 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MYURL = (
-    "https://u1027dltkddb-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
+    "https://u1027dltkddb-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01."
+    "proxy.cognitiveclass.ai"
 )
 ALLOWED_HOSTS = ['localhost', MYURL]
 CSRF_TRUSTED_ORIGINS = [MYURL]
