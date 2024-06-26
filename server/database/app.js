cf34f3be6fb2f7147d5b1515e4e9e1153687696d
SangyunLee1027 <1027dltkddbs@gmail.com>
2024-06-26 08:29:44 -0400

    fixing

--- server/database/app.js
+++ server/database/app.js
@@ -73,7 +73,6 @@ app.get('/fetchDealers/:state', async (req, res) => {
     try{
         const documents = await Dealerships.find({state: req.params.state});
         res.json(documents);
-        
     } catch(error) {
         res.status(500).json({ error: 'Error fetching documents' });
     }
